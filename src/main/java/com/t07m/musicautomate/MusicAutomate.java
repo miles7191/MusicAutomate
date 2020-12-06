6d01f4e11e01610b19b382d3e4f0509718f6dcdd
Matthew Rosato <miles7191@gmail.com>
2020-12-05 19:47:51 -0700

    Suppressed System Console

--- src/main/java/com/t07m/musicautomate/MusicAutomate.java
+++ src/main/java/com/t07m/musicautomate/MusicAutomate.java
@@ -18,6 +18,8 @@ package com.t07m.musicautomate;
 import java.util.logging.Level;
 
 import com.t07m.application.Application;
+import com.t07m.console.Console;
+import com.t07m.console.NativeConsole;
 import com.t07m.musicautomate.command.SetCommand;
 import com.t07m.musicautomate.config.MAConfig;
 import com.t07m.musicautomate.file.source.MusicSource;
@@ -31,15 +33,29 @@ import net.cubespace.Yamler.Config.InvalidConfigurationException;
 public class MusicAutomate extends Application{
 
 	public static void main(String[] args) {
-		new MusicAutomate().start();
+		boolean gui = true;
+		if(args.length > 0) {
+			for(String arg : args) {
+				if(arg.equalsIgnoreCase("-nogui")) {
+					gui = false;
+				}
+			}
+		}
+		new MusicAutomate(gui).start();
 	}
-	
+
+	private final boolean gui;
+
 	private @Getter MAConfig config;
-	private @Getter ConsoleWindow console;
+	private @Getter Console console;
 	private @Getter MusicBuffer musicBuffer;
 	private @Getter MusicPlayer musicPlayer;
 	private @Getter MusicSource musicSource;
-	
+
+	public MusicAutomate(boolean gui) {
+		this.gui = gui;
+	}
+
 	@SuppressWarnings("serial")
 	public void init() {
 		this.config = new MAConfig();
@@ -54,16 +70,27 @@ public class MusicAutomate extends Application{
 			} catch (InterruptedException e1) {}
 			System.exit(-1);
 		}
-		this.console = new ConsoleWindow("Music Automate", "MusicAutomate") {
-			public void close() {
-				stop();
-			}
-		};
+		if(this.gui) {
+			ConsoleWindow cw = new ConsoleWindow("Music Automate") {
+				public void close() {
+					stop();
+				}
+			};
+			cw.setup();
+			cw.setLocationRelativeTo(null);
+			cw.setVisible(true);
+		}else {
+			this.console = new NativeConsole("MusicAutomate") {
+				public void close() {
+					stop();
+				}
+			};
+			this.console.setup();
+		}
 		this.console.getLogger().setLevel(Level.parse(config.getLogger()));
-		this.console.setup();
+		this.console.suppressSystemErr();
+		this.console.suppressSystemOut();
 		this.console.registerCommands(new SetCommand());
-		this.console.setLocationRelativeTo(null);
-		this.console.setVisible(true);
 		this.console.getLogger().info("Launching Application.");
 		this.musicBuffer = new MusicBuffer(this);
 		this.musicPlayer = new MusicPlayer(this);
