a7e16f1b43699d416f981792302adc44326d2c02
Matthew Rosato <miles7191@gmail.com>
2020-12-19 19:32:37 -0700

    Updated ToString method

--- src/main/java/com/t07m/musicautomate/file/AudioFile.java
+++ src/main/java/com/t07m/musicautomate/file/AudioFile.java
@@ -23,52 +23,64 @@ import net.bramp.ffmpeg.probe.FFmpegFormat;
 @ToString
 public class AudioFile {
 
+	@ToString.Exclude
 	private final FFmpegFormat format;
 	
 	AudioFile(FFmpegFormat format){
 		this.format = format;
 	}
 	
+	@ToString.Include
 	public String filename() {
 		return format.filename;
 	}
 	
+	@ToString.Include
 	public int nb_streams() {
 		return format.nb_streams;
 	}
 	
+	@ToString.Include
 	public int nb_programs() {
 		return format.nb_programs;
 	}
 	
+	@ToString.Include
 	public String format_name() {
 		return format.format_name;
 	}
 	
+	@ToString.Include
 	public String format_long_name() {
 		return format.format_long_name;
 	}
 	
+	@ToString.Include
 	public double start_time() {
 		return format.start_time;
 	}
 	
+	@ToString.Include
 	public double duration() {
 		return format.duration;
 	}
 	
+	@ToString.Include
 	public long size() {
 		return format.size;
 	}
 	
+	@ToString.Include
 	public long bit_rate() {
 		return format.bit_rate;
 	}
 	
+	@ToString.Include
 	public int probe_score() {
 		return format.probe_score;
 	}
 	
+	@ToString.Include
 	public Map<String, String> tags(){
 		return format.tags;
 	}
